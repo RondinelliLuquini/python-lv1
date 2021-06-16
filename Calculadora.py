ac62b8067be6b5fde5b58ada524f55153883510d
Rondinelli Luquini <79580178+rondinellivieira@users.noreply.github.com>
2021-06-16 14:14:27 -0300

    Add files via upload

---
+++ Calculadora.py
@@ -0,0 +1,18 @@
+num1 = float(input ("Digte um numero: "))
+num2 = float(input ("Digte outro numero: "))
+operacao = input ("Selecione a operação: + Soma, - Subtracao , * Multiplicacao, / Divisao: ")
+
+if operacao == "+":
+    print(num1 + num2)
+elif operacao == "-":
+    print(num1 + num2)
+elif operacao == "*":
+    print(num1 * num2)
+elif operacao == "/":
+    print(num1 / num2)
+
+else:
+    print("operacao invalida")
+
+
+
